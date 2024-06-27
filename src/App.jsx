9fe7b646197bf755e79b5a7deee16e8e47dfff1b
Shantanu Gurudutt <dsorn@pm.me>
2024-06-27 17:26:47 +0530

    WE GONNA MAKE IT BROS😭😭😭

---
+++ src/App.jsx
@@ -0,0 +1,267 @@
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { LucideChevronsUpDown } from "lucide-react";
+import "./index.css";
+
+function App() {
+  const [chillerIsCollapsed, setChillerIsCollapsed] = useState(true);
+  const [upsIsCollapsed, setUpsIsCollapsed] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://uuhkltdgazctpotmtxom.supabase.co/rest/v1/internal1?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1aGtsdGRnYXpjdHBvdG10eG9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4OTIyMjksImV4cCI6MjAzMjQ2ODIyOX0.btb5VSLKX--4XJgpT9s3hU2n67W5OW7GJKB-BVTWcOU"
+    )
+      .then((response) => {
+        // Log the raw response data
+        console.log("Raw Response Data:", response);
+        return response.json();
+      })
+      .then((data) => {
+        // Log the parsed JSON data
+        console.log("Parsed JSON Data:", data);
+        setData(data);
+      })
+      .catch((error) => console.log(error));
+
+    console.log(data);
+  }, []);
+  const handleChillerCollapseToggle = () => {
+    setChillerIsCollapsed(!chillerIsCollapsed);
+  };
+  const handleUpsCollapseToggle = () => {
+    setUpsIsCollapsed(!upsIsCollapsed);
+  };
+  const handleAllToggle = () => {
+    const newChillerIsCollapsed = chillerIsCollapsed && upsIsCollapsed;
+    const newUpsIsCollapsed = chillerIsCollapsed && upsIsCollapsed;
+    setChillerIsCollapsed(!newChillerIsCollapsed);
+    setUpsIsCollapsed(!newUpsIsCollapsed);
+  };
+  const chillers = [
+    { name: "Chiller 1", status: "running", lastUpdated: "Just now" },
+    {
+      name: "Chiller 2",
+      status: "stopped",
+      lastUpdated: "2 minutes ago",
+    },
+    {
+      name: "Chiller 3",
+      status: "error",
+      lastUpdated: "5 minutes ago",
+    },
+    {
+      name: "Chiller 4",
+      status: "error",
+      lastUpdated: "10 minutes ago",
+    },
+    {
+      name: "Chiller 5",
+      status: "running",
+      lastUpdated: "15 minutes ago",
+    },
+    {
+      name: "Chiller 6",
+      status: "error",
+      lastUpdated: "30 minutes ago",
+    },
+  ];
+  const upsSystems = [
+    { name: "UPS 1", status: "running", lastUpdated: "Just now" },
+    {
+      name: "UPS 2",
+      status: "stopped",
+      lastUpdated: "2 minutes ago",
+    },
+    {
+      name: "UPS 3",
+      status: "error",
+      lastUpdated: "5 minutes ago",
+    },
+    {
+      name: "UPS 4",
+      status: "error",
+      lastUpdated: "10 minutes ago",
+    },
+    {
+      name: "UPS 5",
+      status: "running",
+      lastUpdated: "15 minutes ago",
+    },
+    {
+      name: "UPS 6",
+      status: "running",
+      lastUpdated: "30 minutes ago",
+    },
+    {
+      name: "UPS 7",
+      status: "stopped",
+      lastUpdated: "45 minutes ago",
+    },
+    {
+      name: "UPS 8",
+      status: "running",
+      lastUpdated: "2 hours ago",
+    },
+    {
+      name: "UPS 9",
+      status: "error",
+      lastUpdated: "2 days ago",
+    },
+    {
+      name: "UPS 10",
+      status: "stopped",
+      lastUpdated: "3 days ago",
+    },
+  ];
+  const totalChillers = chillers.length;
+  const totalUPS = upsSystems.length;
+  const allData = [totalChillers, totalUPS];
+  return (
+    <div className="pb-16 min-h-screen bg-gray-800 -z-10">
+      <Navbar
+        allData={allData}
+        chillerHandler={handleChillerCollapseToggle}
+        upsHandler={handleUpsCollapseToggle}
+        allHandler={handleAllToggle}
+      />
+      <div className="pt-16 pl-64 text-white">
+        <div className="flex items-center justify-between px-5 py-3 lg:px-10 lg:pl-5">
+          <h1 className="text-2xl font-semibold">Chillers</h1>
+          <LucideChevronsUpDown
+            size={24}
+            onClick={handleChillerCollapseToggle}
+          />
+        </div>
+        <div className="px-2 pl-1 grid grid-cols-1 sm:grid-cols-3 gap-4">
+          {chillerIsCollapsed &&
+            chillers.map((device, index) => (
+              <div
+                key={device.name}
+                className={`bg-gray-900 rounded-lg shadow p-4 ${
+                  index % 3 === 0 &&
+                  index != 0 &&
+                  index != 3 &&
+                  index != 6 &&
+                  index != 9 &&
+                  index != 12 &&
+                  index != 15 &&
+                  index != 18 &&
+                  index != 21 &&
+                  index != 24 &&
+                  index != 27 &&
+                  index != 30
+                    ? "col-span-3"
+                    : ""
+                } border ${
+                  device.status === "running"
+                    ? "border-green-400"
+                    : device.status === "stopped"
+                    ? "border-gray-400"
+                    : "border-red-400"
+                } `}
+              >
+                <div className="flex items-center justify-between text-lg font-medium text-gray-200">
+                  {device.name}
+                  <div
+                    className={`w-8 h-8 mr-2 rounded-xl ${
+                      device.status === "running"
+                        ? "bg-green-600"
+                        : device.status === "stopped"
+                        ? "bg-gray-600"
+                        : "bg-red-600"
+                    }`}
+                  ></div>
+                </div>
+                <div className="flex items-center justify-end mt-2">
+                  <div
+                    className={`text-sm font-semibold font-mono text-gray-400 ${
+                      device.status === "running"
+                        ? "text-green-400"
+                        : device.status === "stopped"
+                        ? "text-gray-400"
+                        : "px-1 text-red-400"
+                    }`}
+                  >
+                    {device.status}
+                  </div>
+                </div>
+                <div className="mt-2 text-xs text-gray-400">
+                  Last updated {device.lastUpdated}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      {/* UPS Systems */}
+      <div className="pt-6 pl-64 text-white">
+        <div className="flex items-center justify-between px-5 py-3 lg:px-10 lg:pl-5">
+          <h1 className="text-2xl font-semibold">UPS Systems</h1>
+          <LucideChevronsUpDown size={24} onClick={handleUpsCollapseToggle} />
+        </div>
+        <div className=" px-2 grid grid-cols-1 sm:grid-cols-3 gap-4">
+          {upsIsCollapsed &&
+            upsSystems.map((device, index) => (
+              <div
+                key={device.name}
+                className={`bg-gray-900 rounded-lg shadow p-4 ${
+                  index % 3 === 0 &&
+                  index != 0 &&
+                  index != 3 &&
+                  index != 6 &&
+                  index != 9 &&
+                  index != 12 &&
+                  index != 15 &&
+                  index != 18 &&
+                  index != 21 &&
+                  index != 24 &&
+                  index != 27 &&
+                  index != 30
+                    ? "col-span-3"
+                    : ""
+                } border ${
+                  device.status === "running"
+                    ? "border-green-400"
+                    : device.status === "stopped"
+                    ? "border-gray-400"
+                    : "border-red-400"
+                }`}
+              >
+                <div className="flex items-center justify-between text-lg font-medium text-gray-200">
+                  {device.name}
+                  <div
+                    className={`w-8 h-8 mr-2 rounded-xl ${
+                      device.status === "running"
+                        ? "bg-green-600"
+                        : device.status === "stopped"
+                        ? "bg-gray-600"
+                        : "bg-red-600"
+                    }`}
+                  ></div>
+                </div>
+                <div className="flex items-center justify-end mt-2">
+                  <div
+                    className={`text-sm font-semibold font-mono text-gray-400 ${
+                      device.status === "running"
+                        ? "text-green-400"
+                        : device.status === "stopped"
+                        ? "text-gray-400"
+                        : "px-1 text-red-400"
+                    }`}
+                  >
+                    {device.status}
+                  </div>
+                </div>
+                <div className="mt-2 text-xs text-gray-400">
+                  Last updated {device.lastUpdated}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { LayoutPanelTop, Zap, ThermometerSnowflake } from "lucide-react";
+
+const Navbar = (props) => {
+  return (
+    <>
+      <nav className="fixed top-0 z-50 w-full border-b bg-gray-800 border-gray-700">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2 justify-start rtl:justify-end">
+              <LayoutPanelTop size={32} className="text-white" />
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
+                Dashboard
+              </span>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full border-r sm:translate-x-0 bg-gray-800 border-gray-700"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                onClick={props.allHandler}
+                href="#"
+                className="flex items-center p-2  rounded-lg text-white hover:bg-gray-700 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5  transition duration-75 text-gray-400 group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  All Devices
+                </span>
+                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium   rounded-full bg-blue-900 text-blue-300">
+                  {props.allData[0] + props.allData[1]}
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={props.upsHandler}
+                href="#"
+                className="flex items-center p-2  rounded-lg text-white hover:bg-gray-700 group"
+              >
+                <ThermometerSnowflake className="w-5 h-5  transition duration-75 text-gray-400 group-hover:text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap">Chillers</span>
+                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium   rounded-full bg-blue-900 text-blue-300">
+                  {props.allData[0]}
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={props.chillerHandler}
+                href="#"
+                className="flex items-center p-2  rounded-lg text-white hover:bg-gray-700 group"
+              >
+                <Zap className="w-5 h-5  transition duration-75 text-gray-400 group-hover:text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  UPS Systems
+                </span>
+                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium   rounded-full bg-blue-900 text-blue-300">
+                  {props.allData[1]}
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  );
+};
+
+export default Navbar;
